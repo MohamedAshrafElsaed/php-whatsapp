b3aef8feb09917c29123850a43fc55307a76c3ed
Mohamed Elsaed <m.ashraf@converted.in>
2025-10-17 17:09:00 +0300

    fix

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -11,7 +11,7 @@ class SitemapController extends Controller
      */
     public function index(): Response
     {
-        $baseUrl = config('app.url');
+        $baseUrl = rtrim(config('app.url'), '/');
         $now = now()->toIso8601String();
 
         $urls = [
@@ -35,28 +35,22 @@ class SitemapController extends Controller
             ],
         ];
 
-        $xml = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
-        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"' . PHP_EOL;
-        $xml .= '        xmlns:xhtml="http://www.w3.org/1999/xhtml">' . PHP_EOL;
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
 
         foreach ($urls as $url) {
-            $xml .= '  <url>' . PHP_EOL;
-            $xml .= '    <loc>' . htmlspecialchars($url['loc']) . '</loc>' . PHP_EOL;
-            $xml .= '    <lastmod>' . $url['lastmod'] . '</lastmod>' . PHP_EOL;
-            $xml .= '    <changefreq>' . $url['changefreq'] . '</changefreq>' . PHP_EOL;
-            $xml .= '    <priority>' . $url['priority'] . '</priority>' . PHP_EOL;
-
-            // Add alternate language versions
-            $xml .= '    <xhtml:link rel="alternate" hreflang="en" href="' . htmlspecialchars($url['loc'] . '?lang=en') . '" />' . PHP_EOL;
-            $xml .= '    <xhtml:link rel="alternate" hreflang="ar" href="' . htmlspecialchars($url['loc'] . '?lang=ar') . '" />' . PHP_EOL;
-
-            $xml .= '  </url>' . PHP_EOL;
+            $xml .= '  <url>' . "\n";
+            $xml .= '    <loc>' . htmlspecialchars($url['loc'], ENT_XML1, 'UTF-8') . '</loc>' . "\n";
+            $xml .= '    <lastmod>' . $url['lastmod'] . '</lastmod>' . "\n";
+            $xml .= '    <changefreq>' . $url['changefreq'] . '</changefreq>' . "\n";
+            $xml .= '    <priority>' . $url['priority'] . '</priority>' . "\n";
+            $xml .= '  </url>' . "\n";
         }
 
         $xml .= '</urlset>';
 
         return response($xml, 200)
-            ->header('Content-Type', 'application/xml')
+            ->header('Content-Type', 'application/xml; charset=UTF-8')
             ->header('Cache-Control', 'public, max-age=3600');
     }
 }
